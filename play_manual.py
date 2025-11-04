04f64080924e82fad8c224200ca727645494bd33
Sangho <chointer04@gmail.com>
2025-11-04 21:10:33 +0900

    feat: manual mode 실행 파일에 보상 설정 추가

--- play_manual.py
+++ play_manual.py
@@ -8,4 +8,15 @@ import gym_maze
 from gym_maze.utils.play_discrete import play_discrete
 
 env = gym.make('gym_maze/Maze-v0', render_mode="rgb_array", height_range=[5, 20], width_range=[5, 20], manual_mode=True)
+
+rewards = {
+    'goal': (5, None),
+    'friction': (None, 0.01),
+    'manhattan_dist': (0.03, None),
+    'shortest_path': (0.1, None),
+}
+
+for r_name, r in rewards.items():
+    env.reward_manager.add(r_name, r[0], r[1])
+
 play_discrete(env, noop=-1)
