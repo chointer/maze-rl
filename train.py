beee8e69ead7b95024abff707ea71ca91da57481
Sangho <chointer04@gmail.com>
2025-11-14 16:56:38 +0900

    feat: [#9] train.py input 변경, reward 설정, SyncVectorEnv 사용

--- train.py
+++ train.py
@@ -1,42 +1,51 @@
-import gymnasium as gym
-from gymnasium.wrappers.record_episode_statistics import RecordEpisodeStatistics
-from gymnasium.wrappers.autoreset import AutoResetWrapper
-from gymnasium.wrappers.time_limit import TimeLimit
-from stable_baselines3.common.buffers import ReplayBuffer
-
+import random
 import time
 from tqdm import tqdm
-import sys, os
-maze_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'maze')
-if maze_path not in sys.path:
-    sys.path.append(maze_path)
-import gym_maze
+import numpy as np
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.tensorboard import SummaryWriter
-import numpy as np
+
+import gymnasium as gym
+from stable_baselines3.common.buffers import ReplayBuffer
+
+import sys, os
+maze_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'maze')
+sys.path.append(maze_path)
+import gym_maze
+from Evaluator import Evaluator
+
 
 class QNetwork(nn.Module):
-    def __init__(self, env):
+    def __init__(self, obs_shape, n_actions):
         super().__init__()
+        if len(obs_shape) == 4:
+            _, input_h, input_w, input_c = obs_shape
+        elif len(obs_shape) == 3:
+            input_h, input_w, input_c = obs_shape
+        else:
+            raise ValueError(f"Unsupported observation space shape dimension: {len(env.observation_space.shape)}.")
+        input_wh = input_h * input_w
+        
         self.network = nn.Sequential(
-            nn.Conv2d(6, 32, 8, stride=4, padding=4),
+            nn.Conv2d(input_c, 32, 3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(32, 64, 4, stride=2, padding=2),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, 2, stride=1), #nn.Conv2d(64, 64, 3, stride=1),
+            nn.Conv2d(32, 64, 3, padding=1),
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(64, 32),#nn.Linear(256, 128),
+            nn.Linear(input_wh * 64, 512),
             nn.ReLU(),
-            nn.Linear(32, env.action_space.n),#nn.Linear(128, env.action_space.n),
+            nn.Linear(512, n_actions),
         )
 
     def forward(self, x):
-        return self.network(x / 255.0)
+        if x.ndim == 3:
+            x = x.unsqueeze(0)
+        x = x.float().permute(0, 3, 1, 2)
+        return self.network(x)
 
 
 def linear_schedule(start_e: float, end_e: float, duration: int, t: int):
@@ -44,21 +53,54 @@ def linear_schedule(start_e: float, end_e: float, duration: int, t: int):
     return max(slope * t + start_e, end_e)
 
 
-if __name__ == "__main__":
+def make_maze_env(env_id, seed, height_range, width_range, truncation_limit, rewards_dict):
+    def thunk():
+        env = gym.make(env_id, height_range=height_range, width_range=width_range)
+        env = gym.wrappers.TimeLimit(env, truncation_limit)
+        env = gym.wrappers.RecordEpisodeStatistics(env)     # info에 정보가 추가된다. 학습에 영향 없음.
+        #env = gym.wrappers.AutoResetWrapper(env)           # SyncVectorEnv는 기본으로 auto-reset을 적용한다고 함.
+        for r_name, r in rewards_dict.items():
+            env.reward_manager.add(r_name, r[0], r[1])
 
-    ### arguments ###
+        #env = NoopResetEnv(env, noop_max=30)               # Atari game이 결정론적이라 항상 동일 상황에서 시작한다. 그래서 무작위 횟수만큼 noop(no operation?)을 수행하여 환경이 랜덤하게 바뀌게 한다. 미로 환경은 매번 랜덤으로 미로를 생성 및 배치하고, 기다려도 환경이 변하지 않으므로 사용할 필요없다.
+        #env = MaxAndSkipEnv(env, skip=4)                   # 깜빡이는 개체가 보이지 않는 순간으로 인한 문제를 해결하기위한 래퍼. 미로 찾기에는 깜빡이는 개체가 없고, 이미지를 넣지 않을 수도 있기 때문에 사용하지 않는다.
+        #env = EpisodicLifeEnv(env)                         # 목숨이 여러 개인 게임에 대해, 한 번만 죽어도 패널티를 받도록 하는 래퍼. 미로 찾기는 죽는 개념이 없으므로 사용하지 않는다.
+        #if "FIRE" in env.unwrapped.get_action_meanings():  # FIRE 버튼을 눌러야 시작하는 일부 atari game에 사용하는 래퍼
+        #    env = FireResetEnv(env)
+        #env = ClipRewardEnv(env)                           # 게임마다 보상 스케일이 달라서 모든 reward를 -1, 0, 1로 정규화. 미로 찾기는 보상을 설계할 것이므로 사용 X
+        #env = gym.wrappers.ResizeObservation(env, (84, 84))
+        #env = gym.wrappers.GrayScaleObservation(env)
+        #env = gym.wrappers.FrameStack(env, 4)
+
+        env.action_space.seed(seed)
+        return env
+
+    return thunk
+
+
+if __name__ == "__main__":
+    # ====================
+    #      Arguments
+    # ====================
+    env_id = "gym_maze/Maze-v0"
     run_name = "first_test"
+    exp_name = "dqn_00"
     seed = 42
-    run_name = "maze_seed42_1"
+    torch_deterministic = True
     track = False
     cuda = False
+    num_envs = 1            # 1만 가능
 
+    # arguments: Env
+    height_range = [3, 7]
+    width_range = [3, 7]
+    
     learning_rate = 1e-4
     buffer_size = 100000
     total_timesteps = 10000000
     learning_starts = 80000
     train_frequency = 4
-    trunctaion_limit = 1000
+    truncation_limit = 1000
 
     # arguments: Epsilon Greedy
     start_e = 1
@@ -71,82 +113,141 @@ if __name__ == "__main__":
     target_network_frequency = 1000
     tau = 1.0
 
-    ### Initialize ###
-    # if track: import wandb
-    
+    # arguments: Evaluation
+    n_episode_eval = 10
+
+    # ====================
+    #      Initialize
+    # ====================
+    run_name = f"{env_id}__{exp_name}__{seed}"
+
+    # ===== Tensorboard =====
     writer = SummaryWriter(f"runs/{run_name}")
-    
-    # Seed
-    #random.seed(seed)
+    # writer.add_text(
+    #     "hyperparameters",
+    #     "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
+    # )
+
+    # ===== Seed =====
+    random.seed(seed)
     np_rng = np.random.default_rng(seed=seed)
     torch.manual_seed(seed)
-    #torch.backends.cudnn.deterministic = ?
+    torch.backends.cudnn.deterministic = torch_deterministic   # ?
 
     device = torch.device("cuda" if torch.cuda.is_available() and cuda else "cpu")
 
-    # Initialize: Env
-    env = gym.make('gym_maze/Maze-v0', render_mode="rgb_array", height_range=[5, 10], width_range=[5, 10])
-    env = TimeLimit(env, trunctaion_limit)
-    env = RecordEpisodeStatistics(env)
-    env = AutoResetWrapper(env)
+    # ===== Init.Env =====
+    rewards = {
+        'goal': (5, None),
+        'friction': (None, 0.01),
+        'manhattan_dist': (0.02, None),
+        'shortest_path': (0.05, None),
+    }
+    envs = gym.vector.SyncVectorEnv([
+        make_maze_env(
+            env_id, 
+            seed + i, 
+            height_range=height_range, 
+            width_range=width_range, 
+            truncation_limit=truncation_limit, 
+            rewards_dict=rewards
+        ) for i in range(num_envs)
+    ])
+    assert isinstance(envs.single_action_space, gym.spaces.Discrete), "only discrete action space is supported"
 
-    # Initialize: Networks
-    q_network = QNetwork(env).to(device)
+    # ===== Init.Env_eval =====
+    rewards_eval = {
+        'goal': (5, None),
+        'friction': (None, 0.01),
+    }
+    env_eval = gym.make(env_id, render_mode="rgb_array", height_range=height_range, width_range=width_range)
+    env_eval = gym.wrappers.TimeLimit(env_eval, truncation_limit)
+    for r_name, r in rewards_eval.items():
+        env_eval.reward_manager.add(r_name, r[0], r[1])
+    evaluator = Evaluator(env_eval)
+    
+    # ===== Init.Networks =====
+    obs_shape = envs.single_observation_space.shape     # Shape(H, W, 4 + 2)
+    n_actions = envs.single_action_space.n              # if hasattr(envs, "single_action_space") else envs.action_space.n
+    q_network = QNetwork(obs_shape, n_actions).to(device)
     optimizer = optim.Adam(q_network.parameters(), lr=learning_rate)
-    target_network = QNetwork(env).to(device)
+    target_network = QNetwork(obs_shape, n_actions).to(device)
     target_network.load_state_dict(q_network.state_dict())
     
-    # Initialize: Replay Buffer
+    # ===== Init.ReplayBuffer =====
     rb = ReplayBuffer(
         buffer_size,                   # Buffer Size
-        env.observation_space,      # Observation Space
-        env.action_space,           # Action Space
+        envs.single_observation_space,      # Observation Space
+        envs.single_action_space,           # Action Space
         device,
         optimize_memory_usage=True,
         handle_timeout_termination=False,
     )
+
+
+    # ====================
+    #         Run
+    # ====================
     start_time = time.time()
 
-    obs, _ = env.reset(seed=seed)       # return obs, info
+    obs, _ = envs.reset(seed=seed)       # return obs, info
 
     for global_step in tqdm(range(total_timesteps)):
-        ### Env. Interaction ###
+        # ===== Action (Epsilon Greedy) =====
         epsilon = linear_schedule(start_e, end_e, exploration_fraction * total_timesteps, global_step)      # 전체 학습 스텝의 10% 동안 start_e에서 end_e까지 선형으로 변한다.
         if np_rng.random() < epsilon:
-            action = env.action_space.sample()
+            actions = np.array([envs.single_action_space.sample() for _ in range(envs.num_envs)])
         else:
-            obs_tensor = torch.from_numpy(obs).float().permute(2, 0, 1).unsqueeze(0).to(device)
-            q_value = q_network(obs_tensor)
-            action = torch.argmax(q_value, dim=1).item()
-            action -= 1
+            q_value = q_network(torch.Tensor(obs).to(device))
+            actions = torch.argmax(q_value, dim=1).cpu().numpy()
+            actions -= 1
             # TODO. action은 -1부터 시작하는데, Qnetwork에서 행동 index는 0부터 시작한다. 지금은 index에 1을 빼줌으로써 맞춰줬지만, 나중에 환경에서 행동이 0부터 시작하게 수정하면 편할 것이다.
 
-        # Execute the game
-        next_obs, reward, termination, truncation, info = env.step(action)   # np.ndarray, float, bool, bool, dict
+        # ===== Step =====
+        next_obs, rewards, terminations, truncations, infos = envs.step(actions)   # np.ndarray, float, bool, bool, dict
 
-        # Episode End Handling
-        if "final_info" in info:
-            #print(f"global_step={global_step}, episodic_return={info['final_info']['episode']['r']}, moves={info['final_info']['move_count']}, newsize={env.maze_width}")
-            writer.add_scalar("charts/episodic_return", info['final_info']["episode"]["r"], global_step)
-            writer.add_scalar("charts/episodic_length", info['final_info']["episode"]["l"], global_step)
+        # ===== Record ===== Episode End Handling
+        if "final_info" in infos:
+            """ final_info가 있을 때의 info sample
+            {
+                'move_count': array([0]), '_move_count': array([ True]), 
+                'agent_location': array([array([0, 3])], dtype=object), '_agent_location': array([ True]), 
+                'target_location': array([array([0, 2])], dtype=object), '_target_location': array([ True]), 
+                'final_observation': array([array([[[...]]], dtype=float32)], dtype=object), '_final_observation': array([ True]), 
+                'final_info': array([
+                    {
+                        'move_count': 89,
+                        'agent_location': array([2, 2]), 
+                        'target_location': array([2, 2]), 	
+                        'manual_mode': False, 
+                        'rewards': {'goal': 5, 'friction': -0.01, 'manhattan_dist': 0.02, 'shortest_path': 0.05}, 
+                        'episode': {'r': array([4.55], dtype=float32), 'l': array([89], dtype=int32), 't': array([0.011475], dtype=float32)}
+                    }
+                ], dtype=object), 
+                '_final_info': array([ True])
+            }
+            """
+            for info in infos["final_info"]:
+                if info and "episode" in info:
+                    print(f"global_step={global_step}, episodic_return={info['episode']['r']}, moves={info['move_count']}")
+                    writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
+                    writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)
 
-        real_next_obs = next_obs.copy() if not truncation else info["final_observation"]
-        # trunctation은 강제 종료한 상황이므로 next_obs를 사용하여 target Q를 계산해야한다. 때문에, 실제 final_observation을 가져온다. 
-        # termination은 실제 환경이 종료된 상황으로, target Q 계산에 next_obs를 사용하지 않는다. 그래서 따로 final_observation으로 바꿔주지 않는 것 같다.
+        real_next_obs = next_obs.copy()
+        for idx, trunc in enumerate(truncations):
+            if trunc:
+                real_next_obs[idx] = infos["final_observation"][idx]
+                # truncation은 강제 종료한 상황이므로 next_obs를 사용하여 target Q를 계산해야한다. 때문에, 실제 final_observation을 가져온다. 
+                # termination은 실제 환경이 종료된 상황으로, target Q 계산에 next_obs를 사용하지 않는다. 그래서 따로 final_observation으로 바꿔주지 않는 것 같다.
 
-        # replay buffer
-        if isinstance(action, int):
-            action = np.array([[action]])
-        if isinstance(reward, float):
-            reward = np.array([[reward]])
-        if isinstance(termination, bool):
-            termination = np.array([[termination]])
+        # ===== replay buffer =====
+        rb.add(obs, real_next_obs, actions, rewards, terminations, infos)
 
-        rb.add(obs, real_next_obs, action, reward, termination, info)       # add(obs, next_obs, action, reward, done, infos). truncation으로 끝나면, next_obs를 사용해서 target Q를 계산해야하므로, done에는 truncation만 반영하는 같다.
-        
         obs = next_obs
 
-        ### Train ###
+        # ====================
+        #       Train
+        # ====================
         if global_step > learning_starts:
             if global_step % train_frequency == 0:
                 data = rb.sample(batch_size)
@@ -163,8 +264,7 @@ if __name__ == "__main__":
                 if global_step % 100 == 0:
                     writer.add_scalar("losses/td_loss", loss, global_step)
                     writer.add_scalar("losses/q_values", old_val.mean().item(), global_step)
-                    writer.add_scalar("charts/SPS", int(global_step / (time.time() - start_time)))
-                
+                    
                 # Optimze
                 optimizer.zero_grad()
                 loss.backward()
@@ -177,5 +277,5 @@ if __name__ == "__main__":
                         tau * q_network_param.data + (1.0 - tau) * target_network_param.data
                     )
     
-    env.close()
+    envs.close()
     writer.close()
